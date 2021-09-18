22b2a34403a3939534680b80cfcffc502c8ec753
Beni <benitx126@gmail.com>
2021-09-18 19:45:01 +0200

    RWHandler #4

--- src/com/dusza/RWHandler.java
+++ src/com/dusza/RWHandler.java
@@ -1,64 +1,90 @@
 package com.dusza;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class RWHandler {
-    private String path;
 
     // constructors
-    public RWHandler(String path) {
-        this.path = path;
+    public RWHandler() {
+
     }
 
     // Methods
 
-    private String readFile(String p){
-        //try(Scanner scanner = new Scanner())
-
-        try {
-            File f = new File(path + p);
-            Scanner myReader = new Scanner(f);
-            List<String> data = new ArrayList<String>();
-
-            while (myReader.hasNextLine())
-            {
-                data.add(myReader.nextLine());
-            }
-            myReader.close();
-        }
-        catch (FileNotFoundException e)
+    private List<String> readFile(String file){
+        Path path = FileSystems.getDefault().getPath("Data", file);
+        List<String> data = new ArrayList<>();
+        try(Scanner input = new Scanner(Files.newBufferedReader(path)))
         {
-            System.out.println("File named " + p + " not found.");
+
+            while(input.hasNextLine()) {
+                data.add(input.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("File named " + file + " not found.");
         }
-        return "";
+
+        return data;
     }
 
-    public List<Email> readMailsFromUser(User user)
-    {
-        List<Email> mails = new ArrayList<Email>();
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
 
-        return mails;
-    }
+        List<String> data = readFile("adatok.txt");
 
-    public List<User> getUsers()
-    {
-        List<User> users = new ArrayList<User>();
+        for (String line : data)
+        {
+            String[] splt = line.split(" ");
+            // String username, int password, String passwordReminder
+            if (splt.length == 2) {
+                users.add(new User(splt[0], Integer.parseInt(splt[1]), ""));
+            } else {
+                users.add(new User(splt[0], Integer.parseInt(splt[1]), splt[2]));
+            }
+        }
 
         return users;
     }
 
-    // getters & setters
-    public String getPath() {
-        return path;
+    public List<Email> readMailsFromUser(User user) throws ParseException {
+        List<Email> mails = new ArrayList<>();
+
+        List<String> data = readFile(user.getUsername()+".txt");
+
+        for (String line : data)
+        {
+            String[] splt = line.split(" ");
+            // String senderEmailAddress, String object, Date receivedDate, boolean isRead
+            Date date = new SimpleDateFormat("MM.dd").parse(splt[2]);
+
+            if (Objects.equals(splt[3], "olvasott"))
+            {
+                mails.add(new Email(splt[0], splt[1], date, true));
+            } else {
+                mails.add(new Email(splt[0], splt[1], date, false));
+            }
+
+        }
+
+        return mails;
     }
 
-    public void setPath(String path) {
-        this.path = path;
+    public void addUser(User user) {
+        if (Objects.equals(user.getPasswordReminder(), "")) {
+            String line = user.getUsername() + " " + user.getPassword();
+        } else {
+            String line = user.getUsername() + " " + user.getPassword() + " " + user.getPasswordReminder();
+        }
     }
+
+    // getters & setters
+
 
 
 }
