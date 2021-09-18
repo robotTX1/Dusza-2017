2630691ef4fb90738eba6539d374e9e67edb0f9b
Beni <benitx126@gmail.com>
2021-09-18 20:15:40 +0200

    RWHandler #5

--- src/com/dusza/RWHandler.java
+++ src/com/dusza/RWHandler.java
@@ -1,5 +1,7 @@
 package com.dusza;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -9,16 +11,26 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class RWHandler {
+    public static RWHandler single_instance = null;
+
+    Path RWPath;
 
     // constructors
-    public RWHandler() {
-
+    private RWHandler(Path path) {
+        RWPath = FileSystems.getDefault().getPath("Data");
     }
 
     // Methods
 
+    public RWHandler getInstance(Path path) {
+        if (single_instance == null)
+            single_instance = new RWHandler(path);
+
+        return single_instance;
+    }
+
     private List<String> readFile(String file){
-        Path path = FileSystems.getDefault().getPath("Data", file);
+        Path path = RWPath.resolve(file);
         List<String> data = new ArrayList<>();
         try(Scanner input = new Scanner(Files.newBufferedReader(path)))
         {
@@ -42,17 +54,21 @@ public class RWHandler {
         {
             String[] splt = line.split(" ");
             // String username, int password, String passwordReminder
+            User tempUser;
             if (splt.length == 2) {
-                users.add(new User(splt[0], Integer.parseInt(splt[1]), ""));
+                tempUser= new User(splt[0], Integer.parseInt(splt[1]), "");
             } else {
-                users.add(new User(splt[0], Integer.parseInt(splt[1]), splt[2]));
+                tempUser = new User(splt[0], Integer.parseInt(splt[1]), splt[2]);
             }
+
+            tempUser.setEmailList(readEmailsFromUser(tempUser));
+            users.add(tempUser);
         }
 
         return users;
     }
 
-    public List<Email> readMailsFromUser(User user) throws ParseException {
+    public List<Email> readEmailsFromUser(User user) {
         List<Email> mails = new ArrayList<>();
 
         List<String> data = readFile(user.getUsername()+".txt");
@@ -61,13 +77,19 @@ public class RWHandler {
         {
             String[] splt = line.split(" ");
             // String senderEmailAddress, String object, Date receivedDate, boolean isRead
-            Date date = new SimpleDateFormat("MM.dd").parse(splt[2]);
 
-            if (Objects.equals(splt[3], "olvasott"))
-            {
-                mails.add(new Email(splt[0], splt[1], date, true));
-            } else {
-                mails.add(new Email(splt[0], splt[1], date, false));
+            try {
+                Date date = new SimpleDateFormat("MM.dd").parse(splt[2]);
+
+                if (Objects.equals(splt[3], "olvasott"))
+                {
+                    mails.add(new Email(splt[0], splt[1], date, true));
+                } else {
+                    mails.add(new Email(splt[0], splt[1], date, false));
+                }
+
+            } catch (ParseException e) {
+                System.out.println("Date can't be converted: " + splt[2]);
             }
 
         }
@@ -75,11 +97,23 @@ public class RWHandler {
         return mails;
     }
 
-    public void addUser(User user) {
+    public void saveUser(User user) {
+        //
+        String line;
         if (Objects.equals(user.getPasswordReminder(), "")) {
-            String line = user.getUsername() + " " + user.getPassword();
+            line = "\n" + user.getUsername() + " " + user.getPassword();
         } else {
-            String line = user.getUsername() + " " + user.getPassword() + " " + user.getPasswordReminder();
+            line = "\n" + user.getUsername() + " " + user.getPassword() + " " + user.getPasswordReminder();
+        }
+
+        // save the new line to the file
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("adatok.txt", true));
+            writer.append(line);
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Adatok.txt nem létezik");
         }
     }
 
